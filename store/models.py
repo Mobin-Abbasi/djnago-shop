438252aebb2bec112000b06d7a9ab1d8336e0d08
Mobin <mobin04dev@gmail.com>
2024-06-27 20:29:23 +0330

    Create Product model

--- store/models.py
+++ store/models.py
@@ -25,12 +25,24 @@ class ProductFeature(models.Model):
         return f"{self.name}: {self.value}"
 
 
-# class Product(models.Model):
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-#     name = models.CharField(max_length=155)
-#     description = models.TextField()
-#     slug = models.SlugField(max_length=155)
-#     inventory = models.PositiveIntegerField(default=0)
-#     price = models.PositiveIntegerField(default=0)
-#     off = models.PositiveIntegerField(default=0)
-#     new_price = models.PositiveIntegerField(default=0)
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=155)
+    description = models.TextField()
+    slug = models.SlugField(max_length=155)
+    inventory = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+    off = models.PositiveIntegerField(default=0)
+    new_price = models.PositiveIntegerField(default=0)
+    feature = models.ForeignKey(ProductFeature, on_delete=models.CASCADE, related_name="features")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+
+    def __str__(self):
+        return self.name
