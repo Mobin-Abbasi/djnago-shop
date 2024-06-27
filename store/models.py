bb6b26c8d6afc64b67450b1a1f8f97e9ff8603ea
Mobin <mobin04dev@gmail.com>
2024-06-27 19:58:58 +0330

    create categories model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=155)
+    slug = models.SlugField(max_length=155, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+
+    def __str__(self):
+        return self.name
