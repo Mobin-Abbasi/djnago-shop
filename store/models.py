89363e36bda7321bd954b863a2f1e5a7ca98297a
Mobin <mobin04dev@gmail.com>
2024-06-27 20:23:24 +0330

    Create ProductFeature model

--- store/models.py
+++ store/models.py
@@ -15,3 +15,22 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductFeature(models.Model):
+    name = models.CharField(max_length=155)
+    value = models.CharField(max_length=155)
+
+    def __str__(self):
+        return f"{self.name}: {self.value}"
+
+
+# class Product(models.Model):
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+#     name = models.CharField(max_length=155)
+#     description = models.TextField()
+#     slug = models.SlugField(max_length=155)
+#     inventory = models.PositiveIntegerField(default=0)
+#     price = models.PositiveIntegerField(default=0)
+#     off = models.PositiveIntegerField(default=0)
+#     new_price = models.PositiveIntegerField(default=0)
