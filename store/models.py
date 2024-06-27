0e854669a2af8c62f066a39700b9dc098622fae6
Mobin <mobin04dev@gmail.com>
2024-06-27 20:55:35 +0330

    Update models

--- store/models.py
+++ store/models.py
@@ -17,14 +17,6 @@ class Category(models.Model):
         return self.name
 
 
-class ProductFeature(models.Model):
-    name = models.CharField(max_length=155)
-    value = models.CharField(max_length=155)
-
-    def __str__(self):
-        return f"{self.name}: {self.value}"
-
-
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=155)
@@ -34,18 +26,28 @@ class Product(models.Model):
     price = models.PositiveIntegerField(default=0)
     off = models.PositiveIntegerField(default=0)
     new_price = models.PositiveIntegerField(default=0)
-    feature = models.ForeignKey(ProductFeature, on_delete=models.CASCADE, related_name="features")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created']
         indexes = [
-            models.Index(fields=['-created'])
+            models.Index(fields=['-created']),
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
         ]
 
     def __str__(self):
         return self.name
+
+
+class ProductFeature(models.Model):
+    name = models.CharField(max_length=155)
+    value = models.CharField(max_length=155)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="features")
+
+    def __str__(self):
+        return f"{self.name}: {self.value}"
 
 
 class Image(models.Model):
