db69e5c81bd347d7438997423f567684d5959bdc
Mobin <mobin04dev@gmail.com>
2024-06-30 19:38:57 +0330

    create index template and debug store urls and project urls

--- shop/urls.py
+++ shop/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls', namespace='shop')),
+    path('', include('store.urls', namespace='shop')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ app_name = 'shop'
 
 urlpatterns = [
     path('products/', views.product_list, name='product_list'),
-    path('products/<alug:category_slug>/', views.product_list, name='product_list_by_category'),
-    path('product/<id:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('products/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
