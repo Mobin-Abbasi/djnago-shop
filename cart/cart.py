bd0ed1f6ec15e4d764d9f857730f0a39538138dd
Mobin <mobin04dev@gmail.com>
2024-07-03 17:14:56 +0330

    feat: add decrease, remove, post total, total price, __len__, __iter__

--- cart/cart.py
+++ cart/cart.py
@@ -17,5 +17,42 @@ class Cart:
             self.cart[product_id]['quantity'] += 1
         self.save()
 
+    def decrease(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart and self.cart[product_id]['quantity'] > 1:
+            self.cart[product_id]['quantity'] -= 1
+        self.save()
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+        self.save()
+
+    def get_post_price(self):
+        weight = sum(item['weight'] * item['quantity'] for item in self.cart.values())
+        if weight < 1000:
+            return 20000
+        elif 1000 < weight < 2000:
+            return 30000
+        else:
+            return 50000
+
+    def get_total_price(self):
+        price = sum(item['new_price'] * item['quantity'] for item in self.cart.values())
+        return price
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart_dict = self.cart.copy()
+        for product in products:
+            cart_dict[str(product.id)]['product'] = product
+        for item in cart_dict.values():
+            yield item
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
     def save(self):
         self.session.modified = True
