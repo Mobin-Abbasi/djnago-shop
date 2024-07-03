f094c70141221b9b2957dd774c00f8fc67d0147d
Mobin <mobin04dev@gmail.com>
2024-07-03 14:36:22 +0330

    feat: create cart to cart app and coding initial variable

---
+++ cart/cart.py
@@ -0,0 +1,13 @@
+from store.models import Product
+
+
+class Cart:
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get('cart')
+        if not cart:
+            cart = self.session['cart'] = {}
+        self.cart = cart
+
+    def save(self):
+        self.session.modified = True
