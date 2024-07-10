69cd40c6063e0cd08cabe9fc0480fa6c9f5ac60c
Mobin <mobin04dev@gmail.com>
2024-07-10 11:13:22 +0330

    feat: coding order item model and migrate to database

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from store.models import Product
 
 
 # Create your models here.
@@ -24,4 +25,33 @@ class Order(models.Model):
         return f'Order #{self.id}'
 
     def get_total_cost(self):
-        return sum(self.items.cost for item in self.items.all())
+        return sum(item.get_costs() for item in self.items.all())
+
+    def get_post_cost(self):
+        weight = sum(item.get_weight() for item in self.items.all())
+        if weight < 1000:
+            return 20000
+        elif 1000 < weight < 2000:
+            return 30000
+        else:
+            return 50000
+
+    def get_final_cost(self):
+        return self.get_total_cost() + self.get_post_cost()
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=1)
+    weight = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_costs(self):
+        return self.price * self.quantity
+
+    def get_weight(self):
+        return self.weight * self.quantity
