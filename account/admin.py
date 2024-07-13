52481dd3fb3df28810700c22205d28c16936e2e5
Mobin <mobin04dev@gmail.com>
2024-07-13 10:00:30 +0330

    feat: personalization admin form

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 from django.contrib.auth.admin import UserAdmin
+from .forms import UserCreationForm, UserChangeForm
 
 
 # Register your models here.
@@ -8,8 +9,19 @@ from django.contrib.auth.admin import UserAdmin
 @admin.register(ShopUser)
 class ShopUserAdmin(UserAdmin):
     ordering = ['phone']
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = ShopUser
     list_display = ['phone', 'first_name', 'last_name', 'is_staff', 'is_active']
-    fieldsets = UserAdmin.fieldsets + (
-        (None,
-         {'fields': ('address', )}),
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'address',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important date', {'fields': ('last_login', 'date_joined',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'address',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important date', {'fields': ('last_login', 'date_joined',)}),
     )
