b03471815b1be6a4c1770a66f37291da41aab283
Mobin <mobin04dev@gmail.com>
2024-07-13 10:12:52 +0330

    chore: update admin add_fieldset

--- account/admin.py
+++ account/admin.py
@@ -20,7 +20,7 @@ class ShopUserAdmin(UserAdmin):
         ('Important date', {'fields': ('last_login', 'date_joined',)}),
     )
     add_fieldsets = (
-        (None, {'fields': ('phone', 'password')}),
+        (None, {'fields': ('phone', 'password1', 'password2')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'address',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important date', {'fields': ('last_login', 'date_joined',)}),
