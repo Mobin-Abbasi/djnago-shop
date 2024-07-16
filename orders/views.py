fc8042c086362af84bed3e17946c367c1e4be589
Mobin <mobin04dev@gmail.com>
2024-07-17 00:09:03 +0330

    feat: coding order_detail view and template

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.http import HttpResponse
 from django.contrib import messages
@@ -177,4 +177,5 @@ def orders_list(request):
 
 
 def order_detail(request, id):
-    pass
+    order = get_object_or_404(Order, id=id)
+    return render(request, 'order_detail.html', {'order': order})
